79dbb8d6e4fbfc43b9eaaa9ad0533b72ea7592c5
SamGoessling <samwyz@icloud.com>
2024-01-11 19:37:16 -0600

    breakpoints, watch, debugging

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -1,15 +1,19 @@
 package org.launchcode;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //--breakpoint 1--//
         //--constructor calls w/ required parameters--//
         CD myCd = new CD("My CD", 0.7, "Music Album", "Artist Name", 1999);
         DVD myDvd = new DVD("My DVD", 4.7, "Movie", "Director Name", 2008);
 
+        //--breakpoint 2--//
         //--behaviors--//
         myCd.spinDisc();
         myDvd.spinDisc();
 
+        //--breakpoint 3--//
         //--some additional behaviors--//
         myCd.writeDataUsingLaser();
         myDvd.startPlaying();
